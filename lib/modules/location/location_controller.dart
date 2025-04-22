40e83c3353d740fd218eb0e044ba4f188c297861
Krumar25 <80907925+Krumar25@users.noreply.github.com>
2025-04-22 16:03:16 +0200

    temp

--- lib/modules/location/location_controller.dart
+++ lib/modules/location/location_controller.dart
@@ -1,5 +1,31 @@
 import 'package:get/get.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LocationController extends GetxController {
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
+    // Verifica si el servicio está activado
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('La localización está desactivada.');
+    }
+
+    // Verifica los permisos
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Permiso denegado');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Permiso denegado permanentemente');
+    }
+    // Obtén la posición
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high
+    );
+  }
 }
--- lib/modules/location/location_screen.dart
+++ lib/modules/location/location_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'location_controller.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -11,9 +13,10 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   late GoogleMapController _mapController;
+  final LocationController _locationController = Get.put(LocationController());
 
   final CameraPosition _initialPosition = const CameraPosition(
-    target: LatLng(41.3784, 2.1925), // Coordenadas de Barcelona
+    target: LatLng(41.3784, 2.1925), // Coordenadas de Barcelona (predeterminadas)
     zoom: 12,
   );
 
@@ -24,7 +27,7 @@ class _LocationScreenState extends State<LocationScreen> {
       position: LatLng(41.3784, 2.1925), // Marcador en el centro de Barcelona
       infoWindow: InfoWindow(
         title: 'Canción 1',
-        snippet: 'Descripción de la Canción 1',
+        snippet: 'Javi todavía no he podido jugar TLOU2 =C',
       ),
       icon: BitmapDescriptor.defaultMarker,
     ),
@@ -36,7 +39,6 @@ class _LocationScreenState extends State<LocationScreen> {
         snippet: 'Descripción de la Canción 2',
       ),
       icon: BitmapDescriptor.defaultMarker,
-      //icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
     ),
     const Marker(
       markerId: MarkerId('song3'),
@@ -46,7 +48,6 @@ class _LocationScreenState extends State<LocationScreen> {
         snippet: 'Descripción de la Canción 3',
       ),
       icon: BitmapDescriptor.defaultMarker,
-      //icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
     ),
   };
 
@@ -58,12 +59,37 @@ class _LocationScreenState extends State<LocationScreen> {
       ),
       body: GoogleMap(
         initialCameraPosition: _initialPosition, // Configura la cámara inicial
-        onMapCreated: (controller) {
+        onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
+          _goToUserLocation(); // Llamar a la función después de que se cree el mapa
         },
         mapType: MapType.normal,
         markers: markers,
+        myLocationEnabled: true, // Permite la ubicación actual
+        myLocationButtonEnabled: true, // Activa el botón de ubicación
       ),
     );
+  }
+
+  Future<void> _goToUserLocation() async {
+    try {
+      // Obtén la ubicación actual del usuario
+      final position = await _locationController.determinePosition();
+
+      // Verifica que las coordenadas obtenidas sean correctas
+      print("Ubicación actual: Lat: ${position.latitude}, Lon: ${position.longitude}");
+
+      final LatLng userLatLng = LatLng(position.latitude, position.longitude);
+
+      // Asegúrate de que _mapController esté inicializado antes de usarlo
+      // Mueve la cámara al lugar del usuario
+      _mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: userLatLng, zoom: 15),
+        ),
+      );
+        } catch (e) {
+      print('Error obteniendo ubicación: $e');
+    }
   }
 }
